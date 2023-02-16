90b3f963fbbfd855f60d6f7d619626d9daaeb951
Arkadina <61208486+Arkadina@users.noreply.github.com>
2023-02-16 00:16:13 -0300

    styles with css

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,14 @@
 import Footer from "./Footer";
 import Header from "./Header";
 
+import styles from "./layout.module.css";
+
 export default function Layout({ children }) {
     return (
-        <>
+        <div className={styles.App}>
             <Header />
             <main>{children}</main>
             <Footer />
-        </>
+        </div>
     );
 }
